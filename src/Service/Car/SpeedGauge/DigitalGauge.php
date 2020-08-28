b0a9192d0eea39ecd0433808f672527881b8fea9
Kamil Gnidzinski <k.gnidzinski@gmail.com>
2020-08-28 20:07:13 +0200

    fixed value for unit conversion

--- src/Service/Car/SpeedGauge/DigitalGauge.php
+++ src/Service/Car/SpeedGauge/DigitalGauge.php
@@ -4,7 +4,7 @@ namespace App\Service\Car\SpeedGauge;
 
 class DigitalGauge implements CarSpeedGaugeAbstract
 {
-    private const MILE_IN_KM = 1.609344;
+    private const MILE_IN_KM = 0.621371;
 
     public function getCurrentSpeedReading(int $speed)
     {
