9ae6c7035bd708a3e16a424e1348691aac4ffb92
Kamil Gnidzinski <k.gnidzinski@gmail.com>
2020-08-28 15:50:49 +0200

    added missing changes to CarSpeedMeter

--- src/Service/Car/SpeedMeter/CarSpeedMeter.php
+++ src/Service/Car/SpeedMeter/CarSpeedMeter.php
@@ -13,6 +13,7 @@ class CarSpeedMeter
     private $stop = false;
     private $sum = 0;
     private $result = 0;
+
     /**
      * CarSpeedMeter constructor.
      */
@@ -21,15 +22,54 @@ class CarSpeedMeter
         $this->dataAdapter = $dataAdapter;
     }
 
-    public function getVelocity()
+    public function getAverageSpeed()
     {
-        while($this->stop == false)
-        {
+        while ($this->stop == false) {
             $sample = $this->dataAdapter->getVelocityData($this->second);
             $this->samplesCount++;
             $this->sum += $sample;
-            $this->result = $this->sum/$this->samplesCount;
+            $this->result = $this->sum / $this->samplesCount;
         }
 
+    }
+
+    /**
+     * @param int $samplesCount
+     */
+    public function setSamplesCount(int $samplesCount): void
+    {
+        $this->samplesCount = $samplesCount;
+    }
+
+    /**
+     * @param int $second
+     */
+    public function setSecond(int $second): void
+    {
+        $this->second = $second;
+    }
+
+    /**
+     * @param bool $stop
+     */
+    public function setStop(bool $stop): void
+    {
+        $this->stop = $stop;
+    }
+
+    /**
+     * @param int $sum
+     */
+    public function setSum(int $sum): void
+    {
+        $this->sum = $sum;
+    }
+
+    /**
+     * @param int $result
+     */
+    public function setResult(int $result): void
+    {
+        $this->result = $result;
     }
 }
